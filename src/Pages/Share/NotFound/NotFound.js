4d64d7c4a309f30140d823b0566d17aed0d7ecd9
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-25 23:46:59 +0600

    NotFound

--- src/Pages/Share/NotFound/NotFound.js
+++ src/Pages/Share/NotFound/NotFound.js
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// const NotFound = () => {
-//     return (
-//         <div>
-//             <img className='w-full h-2/4' src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg" alt="" />
-//         </div>
-//     );
-// };
+const NotFound = () => {
+    return (
+        <div>
+            <img className='w-full h-2/4' src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg" alt="" />
+        </div>
+    );
+};
 
-// export default NotFound;
+export default NotFound;
